32f11f4eb351f32331e53741f60d108a8e47c286
GabrielSilva88 <gabrielmg.silva@outlook.com>
2024-11-30 10:14:05 -0300

    Atualização de usuario.

--- src/main/java/br/com/fuctura/Aplication.java
+++ src/main/java/br/com/fuctura/Aplication.java
@@ -24,8 +24,43 @@ public class Aplication {
 		
 		System.out.print("Nome: " + userResult.toString());
 		
+		// novo usuario
+		Usuario user2 = new Usuario();
+		//user.setCodigo(1L);
+		user2.setNome("Breno");
+		user2.setIdade(98);
+		
+		em.getTransaction().begin();
+		em.persist(user2);
+		em.getTransaction().commit();
+		Usuario userResult2 = em.find(Usuario.class, 1L);
 		//entityManagerFactory.close();
 		
+		// usuario 3
+		
+		Usuario user3 = new Usuario();
+		//user.setCodigo(1L);
+		user.setNome("Jerundio");
+		user.setIdade(8);
+		
+		em.getTransaction().begin();
+		em.persist(user3);
+		em.getTransaction().commit();
+		Usuario userResult3 = em.find(Usuario.class, 1L);
+		
+		// usuario 4
+		
+		Usuario user4 = new Usuario();
+		//user.setCodigo(1L);
+		user.setNome("Silvanydsh");
+		user.setIdade(45);
+		
+		em.getTransaction().begin();
+		em.persist(user4);
+		em.getTransaction().commit();
+		Usuario userResult4 = em.find(Usuario.class, 1L);
+		
+		// 
 		try{
 			
 		} catch (Exception e) {
@@ -34,3 +69,4 @@ public class Aplication {
 
 	}
 }
+ 
